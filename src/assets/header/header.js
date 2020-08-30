8058631787f3e70acb4b506bb2f0341c55acf029
bro-rukman <rukmanaendang99@gmail.com>
2020-08-30 21:18:39 +0700

    modified

--- src/assets/header/header.js
+++ src/assets/header/header.js
@@ -1,1 +1,13 @@
-import React from 'react'
+import React from "react";
+import "./App.css";
+
+function Header() {
+  return (
+    <div className="card">
+      <h2 className="nama">Endang Rukmana</h2>
+      <p className="job">Frontend dev</p>
+    </div>
+  );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+// import Header from "./assets/header/header"
 import * as serviceWorker from "./serviceWorker";
 
+function helloWorld() {
+  return <p> hello functional </p>;
+}
 
-ReactDOM.render( < App / > , document.getElementById("root"));
+ReactDOM.render(<helloWorld />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

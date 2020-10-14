879be889168cf91b93fbaabb4a2d9bd73e0c3bdd
bro-rukman <rukmanaendang99@gmail.com>
2020-10-15 06:56:04 +0700

    add db.json

--- src/assets/component/BlogPost/BlogPost.jsx
+++ src/assets/component/BlogPost/BlogPost.jsx
@@ -1,26 +1,43 @@
 import React, { Component, Fragment } from "react";
 import Post from "../Post/Post";
 import "./BlogPost.css";
+import axios from "axios";
+// const API = "https://jsonplaceholder.typicode.com/posts";
+// const DEFAULT_QUERY = "redux";
 
 class BlogPost extends Component {
   state = {
-    post: [],
+    hits: [],
+  };
+  getPostAPI = () => {
+    axios.get("http://localhost:3004/posts").then((result) => {
+      this.setState({
+        hits: result.data,
+      });
+    });
+  };
+  handleRemove = (data) => {
+    console.log(data);
+    axios.delete('http://localhost:3004/posts/${data}').then((res) => {
+      this.getPostAPI();
+    });
   };
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((json) => {
-        this.setState({
-          post: json,
-        });
-      });
+    // fetch("https://jsonplaceholder.typicode.com/posts")
+    //   .then((response) => response.json())
+    //   .then((json) => {
+    //     this.setState({
+    //       hits: json,
+    //     });
+    //   });
+    this.getPostAPI();
   }
   render() {
     return (
       <Fragment>
         <p className="section-title">Blog post</p>
-        {this.state.post.map((post) => {
-          return <Post key={post.id} title={post.title} desc={post.body} />;
+        {this.state.hits.map((hits) => {
+          return <Post key={hits.id} data={hits} remove={this.handleRemove} />;
         })}
       </Fragment>
     );

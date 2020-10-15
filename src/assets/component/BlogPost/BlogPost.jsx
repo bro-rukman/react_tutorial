9d82cbf33a66363351623d1c40fd2ee3dbb62d93
bro-rukman <rukmanaendang99@gmail.com>
2020-10-15 07:46:39 +0700

    modified json

--- src/assets/component/BlogPost/BlogPost.jsx
+++ src/assets/component/BlogPost/BlogPost.jsx
@@ -4,6 +4,7 @@ import "./BlogPost.css";
 import axios from "axios";
 // const API = "https://jsonplaceholder.typicode.com/posts";
 // const DEFAULT_QUERY = "redux";
+// json-server --watch db.json --port 3004
 
 class BlogPost extends Component {
   state = {
@@ -18,7 +19,7 @@ class BlogPost extends Component {
   };
   handleRemove = (data) => {
     console.log(data);
-    axios.delete('http://localhost:3004/posts/${data}').then((res) => {
+    axios.delete(`http://localhost:3004/posts/${data}`).then((res) => {
       this.getPostAPI();
     });
   };
@@ -36,6 +37,13 @@ class BlogPost extends Component {
     return (
       <Fragment>
         <p className="section-title">Blog post</p>
+        <div className="form-add-post">
+          <label htmlFor="title">Title</label>
+          <input type="text" name="title" placeholder="add title"/>
+          <label htmlFor="body-content">Blog content</label>
+          <textarea name="body-content" id="body-content" cols="30" rows="10" placeholder="add-blog-content"></textarea>
+          <button className="btn-submit">Simpan</button>
+        </div>
         {this.state.hits.map((hits) => {
           return <Post key={hits.id} data={hits} remove={this.handleRemove} />;
         })}

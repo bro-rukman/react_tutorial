fe552c55db81a6e859c3ab1cb955321bdd4d488c
bro-rukman <rukmanaendang99@gmail.com>
2021-01-15 07:03:29 +0700

    finished hooks

--- src/assets/pages/Hooks/Hooks.jsx
+++ src/assets/pages/Hooks/Hooks.jsx
@@ -1,24 +1,48 @@
-import React,{Component} from 'react';
+import React,{Component, useState, useEffect} from 'react';
 import "./Hooks.css";
 
-class Hooks extends Component{
-    state={
-        count : 0,
-    }
-    onUpdate=()=>{
-        this.setState({
-            count: this.state.count + 1
-        })
-    }
-    render(){
-        console.log('count',this);
-        return(
-            <div className="p-hooks">
-                <p>Nilai Saat ini adalah : {this.state.count}</p>
-                <button onClick={this.onUpdate }>Update Nilai</button>
-                {/* 13.45 video 34 */}
+// class Hooks extends Component{
+//     state={
+//         count : 0,
+//     }
+//     onUpdate=()=>{
+//         this.setState({
+//             count: this.state.count +1 
+//         })
+//     }
+//     componentDidMount(){
+//         document.title=`Title change : ${this.state.count}`;
+//     }
+//     componentDidUpdate(){
+//         document.title=`Title change : ${this.state.count}`;
+//     }
+//     componentWillUnmount(){
+//         document.title=`Homepage`;
+//     }
+//     render(){
+//         console.log('count',this);
+//         return(
+//             <div className="p-hooks">
+//                 <p>Nilai Saat ini adalah : {this.state.count}</p>
+//                 <button onClick={this.onUpdate }>Update Nilai</button>
+//             </div>
+//         )
+//     }
+// }
+const Hooks =(props)=>{
+    const [count, setCount]= useState (0);
+
+    useEffect(()=>{
+        document.title=`Title change ${count}`;
+        return()=>{
+            document.title=`Homepage`;
+        }
+    })
+    return(
+        <div className="p-hooks">
+                <p>Nilai Saat ini adalah : {count}</p>
+                <button onClick={()=>setCount(count+1)}>Update Nilai</button>
             </div>
-        )
-    }
+    )
 }
 export default Hooks;
